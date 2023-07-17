3e35ce54c5b3b1a0c01b69da307577e7c65b6fb9
younes-alkashmi <93258554+younes-alkashmi@users.noreply.github.com>
2023-07-17 13:43:01 +0200

    test fa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -113,7 +113,7 @@ function App() {
             element={user ? <UserProfile /> : <Navigate to="../auth" />}
           />
           <Route
-            path="dashboard"
+            path="/dashboard"
             element={admin ? <Dashboard /> : <AdminAuth />}
           />
           <Route
