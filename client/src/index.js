2c6b6d996b371638519d614df23fd2453e93c52b
younes-alkashmi <93258554+younes-alkashmi@users.noreply.github.com>
2023-09-13 10:05:31 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </Provider>,
 
